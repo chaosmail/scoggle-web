5e71085e82ddabc58c3b854d004425de37ee07c0
Christoph Koerner <christoph.koerner@gesim.ch>
2015-05-27 14:44:07 +0200

    Added some security options

--- scoggle/wsgi.py
+++ scoggle/wsgi.py
@@ -11,15 +11,11 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 from django.conf import settings
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "scoggle.settings")
 
 application = get_wsgi_application()
 
-if not settings.DEBUG:
-	
-	# Serve the static file with Django WhiteNoise package
-	# in production
-	from whitenoise.django import DjangoWhiteNoise
-	
-	application = DjangoWhiteNoise(application)
+# Serve the static file with Django WhiteNoise package
+application = DjangoWhiteNoise(application)
